388c9359a658da5fb06d782e6d04961841d18a8c
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-12-14 06:33:43 +0700

    Update DRAMABOX_BASE_URL and add DRAMABOX_TOKEN

--- config/index.js
+++ config/index.js
@@ -5,11 +5,23 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 4000;
 
+/**
+ * Dramabox API
+ */
 export const DRAMABOX_BASE_URL =
-  process.env.DRAMABOX_BASE_URL || 'https://sapi.dramabox.be';
+  process.env.DRAMABOX_BASE_URL || 'https://sementara.site';
 
-export const DRAMABOX_LANG = process.env.DRAMABOX_LANG || 'in';
+export const DRAMABOX_LANG =
+  process.env.DRAMABOX_LANG || 'in';
 
-export const ADSTERRA_DIRECTLINK = process.env.ADSTERRA_DIRECTLINK || '';
+export const DRAMABOX_TOKEN =
+  process.env.DRAMABOX_TOKEN || '';
 
-export const ADSTERRA_FREQ = Number(process.env.ADSTERRA_FREQ || '5');
+/**
+ * Adsterra
+ */
+export const ADSTERRA_DIRECTLINK =
+  process.env.ADSTERRA_DIRECTLINK || '';
+
+export const ADSTERRA_FREQ =
+  Number(process.env.ADSTERRA_FREQ || '5');
